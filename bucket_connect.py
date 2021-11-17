e4e2b5e4ebc22a9750408b43caf84cd9d13ee271
Existentialist-Robot <eredman@ualberta.ca>
2021-11-17 03:23:42 +0000

    cut down bucket_connect.py

--- bucket_connect.py
+++ bucket_connect.py
@@ -1,21 +1,9 @@
-'''
-https://pypi.org/project/google-cloud-storage/
-https://pypi.org/project/firebase-admin/ # takes a while
-'''
-
 from google.cloud import storage
-from firebase_admin import credentials, firestore
-import csv
 
 storage_cli = storage.Client()
 bucket = storage_cli.get_bucket('ml-workshop-123')
-db = firestore.Client()
 
 blob_oddball_path = bucket.get_blob('eeg1.csv')
 oddball_data = blob_oddball_path.download_as_string()
-# oddball_data = oddball_data.decode('utf-8')
+print(oddball_data[0:1000])
 
-f = open('eeg1.csv', 'wb')
-w = csv.writer(f, delimiter = ',')
-w.writerows([x.split(',') for x in oddball_data])
-f.close()
